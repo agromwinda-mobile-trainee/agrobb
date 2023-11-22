6cd0b144b5a63a96d3039af02a9ccd1e9b33672b
DMiyalu <dieudonnemiyalu6@gmail.com>
2023-11-22 11:52:26 +0100

    add prefixIcon

--- lib/widgets/buildbottomsheet.dart
+++ lib/widgets/buildbottomsheet.dart
@@ -59,60 +59,9 @@ Widget destinationFormWidget(context) {
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 22),
-            child: Row(
-              children: [
-                InkWell(
-                  onTap: () => Get.back(),
-                  splashColor: Colors.grey.shade400,
-                  focusColor: Colors.grey.shade400,
-                  hoverColor: Colors.grey.shade400,
-                  highlightColor: Colors.grey.shade400,
-                  child: Ink(
-                    decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: const Icon(
-                      IconlyLight.arrow_left,
-                      color: Colors.black,
-                      size: 22,
-                      semanticLabel: "Retour",
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 12),
-                Text(
-                  "Précisez l'emplacement",
-                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 17,
-                      ),
-                ),
-              ],
-            ),
-          ),
+          destinationFormWidgetHead(context),
           // const SizedBox(height: 20),
-          Container(
-            padding: const EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Colors.grey.shade100.withOpacity(.4),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Point de depart"),
-                Divider(
-                  color: Colors.grey.shade400,
-                  height: 2,
-                ),
-                Text("Destination"),
-              ],
-            ),
-          ),
+          destinationFormWidgetInputFields(context),
         ],
       ),
     ),
@@ -132,6 +81,148 @@ Decoration bottomSheetDecoration(context) {
     borderRadius: const BorderRadius.only(
       topRight: Radius.circular(25),
       topLeft: Radius.circular(25),
+    ),
+  );
+}
+
+Widget destinationFormWidgetHead(context) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 22),
+    child: Row(
+      children: [
+        InkWell(
+          onTap: () => Get.back(),
+          splashColor: Colors.grey.shade400,
+          focusColor: Colors.grey.shade400,
+          hoverColor: Colors.grey.shade400,
+          highlightColor: Colors.grey.shade400,
+          child: Ink(
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: const Icon(
+              IconlyLight.arrow_left,
+              color: Colors.black,
+              size: 22,
+              semanticLabel: "Retour",
+            ),
+          ),
+        ),
+        const SizedBox(width: 12),
+        Text(
+          "Précisez l'emplacement",
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                fontWeight: FontWeight.w500,
+                fontSize: 17,
+              ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget destinationFormWidgetInputFields(context) {
+  return Container(
+    padding: const EdgeInsets.all(15),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(15),
+      color: Colors.grey.shade100.withOpacity(.6),
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        inputField(
+          context,
+          label: "Point de depart",
+          hint: "9, avenue de l'Equateur, Gombe, Kinshasa...",
+          prefixIcon: prefixIconStartPoint(context),
+        ),
+        Divider(
+          color: Colors.grey.shade300.withOpacity(.9),
+          height: 4,
+        ),
+        inputField(context,
+            label: "Destination",
+            hint: "Kitambo, magasin, Ngaliema, Kinshasa...",
+            prefixIcon: prefixIconFinalPoint(context)),
+      ],
+    ),
+  );
+}
+
+Widget prefixIconStartPoint(context) {
+  return Container(
+    height: 20,
+    width: 20,
+    padding: const EdgeInsets.all(4),
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Theme.of(context).colorScheme.primary,
+    ),
+    child: Container(
+      padding: const EdgeInsets.all(1),
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white,
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(1),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+        child: Container(
+            decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+        )),
+      ),
+    ),
+  );
+}
+
+Widget prefixIconFinalPoint(context) {
+  return Container(
+    height: 20,
+    width: 20,
+    decoration: const BoxDecoration(
+      shape: BoxShape.circle,
+      color: Colors.black54,
+    ),
+    child: const Icon(
+      IconlyBold.location,
+      color: Colors.white,
+      size: 14,
+    ),
+  );
+}
+
+Widget inputField(context,
+    {required String label, String? hint, Widget? prefixIcon}) {
+  return Ink(
+    padding: const EdgeInsets.symmetric(vertical: 6),
+    child: TextField(
+      style: Theme.of(context).textTheme.bodyMedium,
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        fillColor: Colors.transparent,
+        filled: false,
+        contentPadding: const EdgeInsets.symmetric(horizontal: 2),
+        labelText: label,
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        prefixIconConstraints:
+            const BoxConstraints(maxHeight: 50, maxWidth: 50),
+        prefixIcon: Row(
+          children: [
+            prefixIcon ?? prefixIconStartPoint(context),
+            const SizedBox(width: 5),
+          ],
+        ),
+        hintText: hint,
+        hintStyle: Theme.of(context).textTheme.bodyMedium,
+      ),
     ),
   );
 }
