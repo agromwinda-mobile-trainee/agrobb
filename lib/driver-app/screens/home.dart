2df7c8fdf3002d9986f83f8fcc1e210d582bf105
DMiyalu <dieudonnemiyalu6@gmail.com>
2023-11-23 20:35:34 +0100

    add details-course widget

--- lib/driver-app/screens/home.dart
+++ lib/driver-app/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:agrobeba/commons/home/authLogic/cubit/login_process_cubit.dart';
+import 'package:agrobeba/customer-app/screens/widgets/destination/cubits/destination_cubit.dart';
 import 'package:agrobeba/customer-app/screens/widgets/widget_build_Tile.dart';
 import 'package:agrobeba/driver-app/screens/cubits/driver_cubit.dart';
 import 'package:flutter/material.dart';
@@ -85,6 +86,30 @@ Widget awaitForCommandes(context) {
       ),
       child: BlocBuilder<DriverCubit, DriverState>(builder: (context, state) {
         List? commandes = state.driver!["commandes"];
+
+        if (state.driver!["acceptedCommande"] != null) {
+          Column(
+            children: [
+              Text(
+                "Vous avez accepté une course",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(height: 20),
+              BlocBuilder<DriverCubit, DriverState>(builder: (context, state) {
+                return Text(
+                  "Identifiant de la commande: ${state.driver!["acceptedCommande"]?["id"] ?? "--"}",
+                  style: Theme.of(context).textTheme.bodyMedium,
+                );
+              }),
+              BlocBuilder<DriverCubit, DriverState>(builder: (context, state) {
+                return Text(
+                  "Numero du téléphone du client: ${state.driver!["acceptedCommande"]?["customer"]["phoneNumber"] ?? "--"}",
+                  style: Theme.of(context).textTheme.bodyMedium,
+                );
+              }),
+            ],
+          );
+        }
 
         if (commandes!.isEmpty) {
           return const Center(
